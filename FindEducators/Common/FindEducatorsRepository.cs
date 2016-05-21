6b5185c3ab8889a1daf3253dbb16d0af2af88e9a
Tayyab Zafar <tayab_zafar@hotmail.com>
2016-05-22 00:42:56 +0500

    Commit : repository
    repository completed

--- FindEducators/Common/FindEducatorsRepository.cs
+++ FindEducators/Common/FindEducatorsRepository.cs
@@ -23,7 +23,39 @@ namespace FindEducators.Common
 
             return degreeType;
         }
+        public static Test GetTestById(int testId)
+        {
+            var test = new Test();
 
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                test = db.Tests.Single(x => x.Id == testId);
+            }
+
+            return test;
+        }
+        public static TestType GetTestTypeById(int testTypeId)
+        {
+            var testType = new TestType();
+
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                testType = db.TestTypes.Single(x => x.Id == testTypeId);
+            }
+
+            return testType;
+        }
+        public static TestLevel GetTestLevekById(int testLevelId)
+        {
+            var testLevel = new TestLevel();
+
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                testLevel = db.TestLevels.Single(x => x.Id == testLevelId);
+            }
+
+            return testLevel;
+        }
         #endregion
 
         #region List
@@ -39,6 +71,39 @@ namespace FindEducators.Common
 
             return degreeTypeList;
         }
+        public static List<Test> GetAllTests()
+        {
+            var testList = new List<Test>();
+
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                testList = db.Tests.ToList();
+            }
+
+            return testList;
+        }
+        public static List<TestType> GetAllTestTypes()
+        {
+            var testTypeList = new List<TestType>();
+
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                testTypeList = db.TestTypes.ToList();
+            }
+
+            return testTypeList;
+        }
+        public static List<TestLevel> GetAllTestLevels()
+        {
+            var testLevelList = new List<TestLevel>();
+
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                testLevelList = db.TestLevels.ToList();
+            }
+
+            return testLevelList;
+        }
         #endregion
 
         #region Insert
@@ -52,7 +117,33 @@ namespace FindEducators.Common
                 db.SaveChanges();
             }
         }
+        public static void InsertTest(Test test)
+        {
 
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                db.Tests.Add(test);
+                db.SaveChanges();
+            }
+        }
+        public static void InsertTestType(TestType testType)
+        {
+
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                db.TestTypes.Add(testType);
+                db.SaveChanges();
+            }
+        }
+        public static void InsertTestLevel(TestLevel testLevel)
+        {
+
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                db.TestLevels.Add(testLevel);
+                db.SaveChanges();
+            }
+        }
         #endregion
 
         #region Update
@@ -62,6 +153,35 @@ namespace FindEducators.Common
             {
                 var tempDegreetype = db.DegreeTypes.Single(x => x.Id == degreeType.Id);
                 tempDegreetype.DegreeTypeName = degreeType.DegreeTypeName;
+                db.SaveChanges();
+            }
+        }
+        public static void UpdateTest(Test test)
+        {
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                var tempTest = db.Tests.Single(x => x.Id == test.Id);
+                tempTest.TestName = test.TestName;
+                tempTest.TestLevelId = test.TestLevelId;
+                tempTest.TestTypeId = test.TestTypeId;
+                db.SaveChanges();
+            }
+        }
+        public static void UpdateTestType(TestType testType)
+        {
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                var tempTesttype = db.TestTypes.Single(x => x.Id == testType.Id);
+                tempTesttype.TestTypeName = testType.TestTypeName;
+                db.SaveChanges();
+            }
+        }
+        public static void UpdateTestLevel(TestLevel testLevel)
+        {
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                var tempTestlevel = db.TestLevels.Single(x => x.Id == testLevel.Id);
+                tempTestlevel.TestLevelName = testLevel.TestLevelName;
                 db.SaveChanges();
             }
         }
@@ -76,10 +196,40 @@ namespace FindEducators.Common
             {
                 var tempDegreeType = db.DegreeTypes.Single(x => x.Id == degreeTypeId);
                 db.DegreeTypes.Remove(tempDegreeType);
+               db.SaveChanges();
+            }
+        }
+        public static void DeleteTest(int testId)
+        {
+
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                var tempTest = db.Tests.Single(x => x.Id == testId);
+                db.Tests.Remove(tempTest);
                 db.SaveChanges();
             }
         }
+        public static void DeleteTestType(int testTypeId)
+        {
 
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                var tempTestType = db.TestTypes.Single(x => x.Id == testTypeId);
+                db.TestTypes.Remove(tempTestType);
+                db.SaveChanges();
+            }
+        }
+        
+        public static void DeleteTestLevel(int testLevelId)
+        {
+
+            using (FindEducatorsContext db = new FindEducatorsContext())
+            {
+                var tempTestLevel = db.TestLevels.Single(x => x.Id == testLevelId);
+                db.TestLevels.Remove(tempTestLevel);
+               db.SaveChanges();
+            }
+        }
         #endregion
 
 
